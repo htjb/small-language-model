defe800a578171d14bbe33a27ee0784490701ac6
htjb <harrytjbevins@yahoo.co.uk>
2025-08-06 09:45:43 +0100

    removing function call

--- slm/bag-of-words.py
+++ slm/bag-of-words.py
@@ -24,8 +24,3 @@ class bag_of_words:
         # unkonwn words will be mapped to -1
         indices = [self.word_to_index.get(word, -1) for word in words]
         return indices
-    
-bow = bag_of_words()  # Create an instance of the bag_of_words class
-line = "Alice was beginning to get very tired of sitting by her sister on the bank."
-vector = bow.codify(line)  # Convert the line into a bag-of-words vector
-print("Bag-of-words vector:", vector)  # Print the bag-of-words vector

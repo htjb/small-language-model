5024bcd9de02abe1c3c0806c05091e5b39a94fc5
htjb <harrytjbevins@yahoo.co.uk>
2025-08-14 18:23:29 +0100

    positional encoding

--- slm/networks.py
+++ slm/networks.py
@@ -1,14 +1,27 @@
 import torch.nn as nn
 import torch
+import math
+
+def sinusoidal_positional_encoding(max_seq_len, embedding_dim):
+    pe = torch.zeros(max_seq_len, embedding_dim)
+    position = torch.arange(0, max_seq_len, dtype=torch.float).unsqueeze(1)
+    div_term = torch.exp(torch.arange(0, embedding_dim, 2).float() * (-math.log(10000.0) / embedding_dim))
+    pe[:, 0::2] = torch.sin(position * div_term) # even indices
+    pe[:, 1::2] = torch.cos(position * div_term) # odd indices
+    return pe  # [max_seq_len, embedding_dim]
 
 
 class Transformer(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, mlp_layers, mlp_dim):
+    def __init__(self, vocab_size, embedding_dim, mlp_layers, mlp_dim,
+                 context_window_size):
         super(Transformer, self).__init__()
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
         self.query = nn.Linear(embedding_dim, embedding_dim)
         self.key = nn.Linear(embedding_dim, embedding_dim)
         self.value = nn.Linear(embedding_dim, embedding_dim)
+
+        self.pos_enc = sinusoidal_positional_encoding(
+            context_window_size, embedding_dim)
 
         self.layers = nn.ModuleList()
         for _ in range(mlp_layers):
@@ -17,7 +30,7 @@ class Transformer(nn.Module):
         
 
     def forward(self, x):
-        x = self.embedding(x)
+        x = self.embedding(x) + self.pos_enc[:x.size(1)]
         query = self.query(x)
         key = self.key(x)
         value = self.value(x)

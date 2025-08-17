ebcabbee6fdcdde4713b7996286f32d038a4a480
htjb <harrytjbevins@yahoo.co.uk>
2025-08-17 16:54:27 +0100

    predict next 10 words just for fun

--- predictions.py
+++ predictions.py
@@ -20,28 +20,26 @@ mlp_dim = hyperparameters['mlp_dim']  # Define the MLP dimension
 context_window_size = hyperparameters['context_window_size']  # Define the context window size
 
 bow = bag_of_words() 
-line = "Alice was beginning"
-vector = bow.codify(line)  
 
 transform = Transformer(vocab_size=len(bow.word_to_index), 
                 embedding_dim=embedding_size, mlp_layers=mlp_layers, mlp_dim=mlp_dim,
-                context_window_size=context_window_size)  # Create an instance of the Transformer class
+                context_window_size=context_window_size, nheads=2)  # Create an instance of the Transformer class
 
 state_dict = torch.load('alice_in_wonderland_model.pth', map_location=torch.device('cpu'))  # Load the state dictionary
 
 transform.load_state_dict(state_dict)  # Load the state dictionary into the model
 
-test_phrase = "how are you"  # Define a test phrase
+test_phrase = "how are you today what is"  # Define a test phrase
 # only need to make pass through the mlp for the last word... will need to think
 # about how to do this in the future
-output = transform(bow.codify(test_phrase).unsqueeze(0))
 
-output = torch.softmax(output, dim=-1)  # Apply softmax to the output
-print(output)
-print("Output shape:", output.shape)  # Print the shape of the output
-# the last ouput is the prediction for the next word
-for j in range(len(test_phrase.split(" "))):
-    out = np.argmax(output[0, j, :].detach().numpy())
+vector = bow.codify(test_phrase)
+for i in range(10):
+    output = transform(vector.unsqueeze(0))
+    out = np.argmax(output[0, -1, :].detach().numpy())
     index_to_word = {i: w for w, i in bow.word_to_index.items()}
     predicted_word = index_to_word[out]
-    print("Predicted words:", predicted_word)  # Print the predicted words
+    print("Predicted word:", predicted_word, " Out:", out)  # Print the predicted word
+    vector = torch.cat((vector, torch.tensor([out])), dim=0)  # Append the predicted word to the vector
+
+

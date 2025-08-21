a8a409550d12dc9dae746299f96e864a72202b61
htjb <harrytjbevins@yahoo.co.uk>
2025-08-21 20:57:22 +0100

    formatting webiste files

--- website/js/main.js
+++ website/js/main.js
@@ -1,9 +1,14 @@
 function loadHTML(elementId, filePath) {
-    fetch(filePath)
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById(elementId).innerHTML = html;
-        });
+  /* 
+    Look for the file.
+    If file exists extract contents as text.
+    Fill HTML element with text.
+    */
+  fetch(filePath)
+    .then((response) => response.text())
+    .then((html) => {
+      document.getElementById(elementId).innerHTML = html;
+    });
 }
 
-loadHTML('welcome-message', 'includes/welcome.html');
+loadHTML("welcome-message", "includes/welcome.html");

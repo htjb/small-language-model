b50f856e24fd852b8418a8853454ac8450befe51
htjb <harrytjbevins@yahoo.co.uk>
2025-09-05 17:16:09 +0100

    responding to input text from user

--- website/js/main.js
+++ website/js/main.js
@@ -1,14 +1,28 @@
-function loadHTML(elementId, filePath) {
-  /* 
-    Look for the file.
-    If file exists extract contents as text.
-    Fill HTML element with text.
-    */
-  fetch(filePath)
-    .then((response) => response.text())
-    .then((html) => {
-      document.getElementById(elementId).innerHTML = html;
-    });
+import { loadHTML } from "./utils.js";
+
+function getInputText() {
+  if (welcomeMessage.innerHTML) {
+    welcomeMessage.innerHTML = "";
+  }
+  history += input.value;
+  console.log(history);
+  let inputDiv = document.createElement("div");
+  inputDiv.setAttribute("class", "input-element");
+  inputDiv.textContent = input.value;
+  document.getElementById("chat-history").appendChild(inputDiv);
 }
 
 loadHTML("welcome-message", "includes/welcome.html");
+
+let history = "";
+let input = document.getElementById("user-input");
+let submit = document.getElementById("submit-button");
+let welcomeMessage = document.getElementById("welcome-message");
+
+submit.addEventListener("click", getInputText);
+input.addEventListener("keydown", function (e) {
+  if (e.keyCode == 13) {
+    console.log("enter pressed");
+    getInputText();
+  }
+});

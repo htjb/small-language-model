4722fafdaa9bec24955f2cc8c7bd8bafa1e83009
htjb <harrytjbevins@yahoo.co.uk>
2025-09-17 17:53:27 +0100

    code to grab simple wikipedia articles

---
+++ model_training/data/wiki/grab-wiki.py
@@ -0,0 +1,14 @@
+from datasets import load_dataset
+from tqdm import tqdm
+
+# This grabs the 20220301 English dump (you can choose a newer date if you like)
+wiki = load_dataset("wikimedia/wikipedia", "20231101.simple", split=None)[
+    "train"
+]
+
+pbar = tqdm(wiki, desc="Processing articles")
+for i, example in enumerate(pbar):
+    with open(f"wiki_{i:05d}.txt", "w", encoding="utf-8") as f:
+        f.write(example["text"])
+    if i >= 1000:  # Limit to first 1000 articles for this example
+        break
